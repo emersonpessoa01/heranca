dc79ee624ba4e5459690839e940b0849a7c11eb2
Emerson Pessoa da Silva <emersonpessoa05102008@gmail.com>
2025-05-30 15:57:10 -0300

    feat: Main updated :rocket:

--- src/main/java/br/com/cdb/heranca/model/Main.java
+++ src/main/java/br/com/cdb/heranca/model/Main.java
@@ -13,12 +13,22 @@ public class Main {
 
         Funcionario funcionario = new Funcionario(null, 0, 0);
         funcionario.nome = "Funcionário 1";
+        System.out.println("Nome do funcionário: " + funcionario.nome);
 
         Professor professor = new Professor(null, 0, 0, 0);
         professor.nome = "Professor 1";
         professor.cpf = 12345678901L;
         professor.salario = 5000.00;
         professor.numeroDeAulas = 10;
+        System.out.println("Nome do professor: " + professor.nome);
+
+        Faxineiro faxineiro = new Faxineiro(null, 0, 0, 0);
+        faxineiro.nome = "Faxineiro 1";
+        faxineiro.cpf = 12345678901L;
+        faxineiro.salario = 2000.00;
+        faxineiro.turno = 1;
+        System.out.println("Nome do faxineiro: " + faxineiro.nome);
+       
         
     }
 }
