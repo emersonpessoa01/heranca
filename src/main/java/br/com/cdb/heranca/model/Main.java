ace1752111bbda4eabf3b86972ac2e37c63250b5
Emerson Pessoa da Silva <emersonpessoa05102008@gmail.com>
2025-05-30 17:55:24 -0300

    feat: Processando poliformicamente com ArrayList :rocket:

--- src/main/java/br/com/cdb/heranca/model/Main.java
+++ src/main/java/br/com/cdb/heranca/model/Main.java
@@ -1,5 +1,7 @@
 package br.com.cdb.heranca.model;
 
+import java.util.ArrayList;
+
 public class Main {
     public static void main(String[] args) {
         Pessoa fulano = new Pessoa(null, 0);
@@ -10,13 +12,13 @@ public class Main {
 
         Aluno aluno1 = new Aluno(null, 0, 0);
         aluno1.nome = "Aluno 1";
-        aluno1.apresentar();
+        // aluno1.apresentar();
 
         Funcionario funcionario = new Funcionario(null, 0, 0);
         funcionario.nome = "Funcionário 1";
         funcionario.cpf = 12345678901L;
         funcionario.salario = 3000.00;
-        funcionario.apresentar();
+        // funcionario.apresentar();
         
 
         Professor professor = new Professor(null, 0, 0, 0);
@@ -25,7 +27,7 @@ public class Main {
         professor.salario = 5000.00;
         professor.numeroDeAulas = 10;
         professor.novoSalario(5500.00, 2); // Sobrecarga de método
-        professor.apresentar();
+        // professor.apresentar();
         
 
         Faxineiro faxineiro = new Faxineiro(null, 0, 0, 0);
@@ -33,7 +35,19 @@ public class Main {
         faxineiro.cpf = 12345678901L;
         faxineiro.salario = 2000.00;
         faxineiro.turno = 1;
-        faxineiro.apresentar();
+        // faxineiro.apresentar();
+
+        ArrayList<Pessoa> listaDaEscola = new ArrayList<>();
+        listaDaEscola.add(fulano);
+        listaDaEscola.add(aluno1);
+        listaDaEscola.add(funcionario);
+        listaDaEscola.add(professor);
+        listaDaEscola.add(faxineiro);
+        
+        for (Pessoa pessoa : listaDaEscola) {
+            pessoa.apresentar();
+        }
+
         
 
     }
